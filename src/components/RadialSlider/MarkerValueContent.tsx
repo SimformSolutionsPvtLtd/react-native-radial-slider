6af91e93d5d63125e08a0959a4440df41d228914
Parth <parthkumar@simformsolutions.com>
2022-06-01 14:18:54 +0530

    feat(UNT-T8496): update readme to include usage example and props information
    feat(UNT-T8496): update readme to include usage example and props

--- src/components/RadialSlider/MarkerValueContent.tsx
+++ src/components/RadialSlider/MarkerValueContent.tsx
@@ -55,20 +55,50 @@ const MarkerValueContent = (
 
         const markerInnerValue = (max / markerValueInterval) as number;
 
+        const centerValue = Math.round((max - min) / 2) as number;
+
+        const getTransformValue = () => {
+          // @ todo : add a prop to transform
+          // if (mark?.value === centerValue) {
+          // return `rotate(185) translate(-5)`
+          // }
+
+          if (mark?.value <= centerValue) {
+            return `rotate(95) translate(-10)`;
+          } else {
+            return `scale(1,1)`;
+          }
+        };
+
+        const getTextPositionValue = (type: string) => {
+          // @ todo : add a prop to transform
+          // if (mark?.value === centerValue) {
+          //   return type === 'x' ? '-100' : '5';
+          // }
+
+          if (mark?.value <= centerValue) {
+            return type === 'x' ? '0' : '-90';
+          } else {
+            return type === 'x' ? '85' : '10';
+          }
+        };
+
         return (
           <G key={index.toString()}>
             {(index % markerInnerValue === 0 || index === markIndex) && (
               <G
                 transform={`translate(${
                   radius + (lineHeight - thumbBorderWidth)
-                }, ${radius + (lineHeight - thumbBorderWidth)})`}>
+                }, ${radius + (lineHeight - thumbBorderWidth)})  `}>
                 {mark.isEven && (
                   <SVGText
-                    transform={`rotate(${index * maxCount + 85 + angle})`}
-                    x="85"
-                    y="10"
+                    transform={`rotate(${
+                      index * maxCount + 87 + angle
+                    })  ${getTransformValue()} `}
+                    x={getTextPositionValue('x')}
+                    y={getTextPositionValue('y')}
                     fill={markerValueColor}
-                    children={mark.value}
+                    children={mark?.value ?? 0}
                   />
                 )}
               </G>
